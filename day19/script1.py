0fc694b688b62ee57179ca3578560652eceb3927
HY <e0726431@u.nus.edu>
2024-12-20 05:32:05 +0800

    day19

---
+++ day19/script1.py
@@ -0,0 +1,29 @@
+import os
+def read_input():
+    path = os.path.dirname(os.path.realpath(__file__))
+    data = open(f"{path}/input.txt", "r").read().strip().split("\n")
+    return data[:data.index("")][0].split(", "), data[data.index("") + 1:]
+left, right = read_input()
+constructible = {l: True for l in left}
+
+def check(can):
+    if can not in constructible:
+        for word in left:
+            if can.endswith(word):
+                remaining = can[:len(can) - len(word)]
+                if check(remaining):
+                    constructible[can] = True
+                    return True
+        constructible[can] = False
+        return False
+    elif not can:
+        return False
+    else:
+        return constructible[can]
+    
+naur = 0
+for candidate in right:
+    if check(candidate):
+        naur+=1
+        
+print(naur)
---
+++ day19/script2.py
@@ -0,0 +1,41 @@
+import os
+def read_input():
+    path = os.path.dirname(os.path.realpath(__file__))
+    data = open(f"{path}/input.txt", "r").read().strip().split("\n")
+    return data[:data.index("")][0].split(", "), data[data.index("") + 1:]
+left, right = read_input()
+
+constructible = {}
+constructible[""] = 0
+def check(can):
+    if can not in constructible:
+        constructible[can] = 0
+        for word in left:
+            if can.endswith(word):
+                remaining = can[:len(can) - len(word)]
+                number = check(remaining)
+                constructible[can] += number
+
+        return constructible[can]
+    elif not can:
+        return 0
+    else:
+        return constructible[can]
+
+ll = sorted([(len(l), l) for l in left], key=lambda x: x[0])
+print(ll)
+minlen = ll[0][0]
+blocks = []
+while ll:
+    length, out = ll.pop(0)
+    if length == minlen:
+        constructible[out] = 1
+    else:
+        constructible[out] = check(out) + 1
+    print((out, constructible[out]))
+
+naur = 0
+for candidate in right:
+    num = check(candidate)
+    naur += num
+print(naur)
