becb1d1aee54f26d98982b9bc5cc170d4e8ab6e0
HY <e0726431@u.nus.edu>
2024-12-20 05:37:35 +0800

    let R shine a lil

--- day1/script1.r
+++ day1/script1.r
@@ -1,12 +1,9 @@
 install.packages("tidyverse")
 library(tidyverse)
-
-
 read_input <- function() {
   data <- read.table("day1/input.txt", header = F, sep = "")
   return(data)
 }
-
 data <- read_input()
 data$V1 <- sort(data$V1, decreasing = F)
 data$V2 <- sort(data$V2, decreasing = F)
